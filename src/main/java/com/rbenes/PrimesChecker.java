4fd097067cf58bf377fd6473a7b756fcc07145c0
Richard Beneš <richard.benes.9@email.cz>
2025-10-30 15:49:37 +0100

    Reformat to java-style; rename App and AppTest

---
+++ src/main/java/com/rbenes/PrimesChecker.java
@@ -0,0 +1,9 @@
+package com.rbenes;
+
+public class PrimesChecker {
+
+    public static void main( String[] args ) {
+
+        System.out.println( "Hello Prime World!" );
+    }
+}
--- src/test/java/com/rbenes/PrimesCheckerTest.java
+++ src/test/java/com/rbenes/PrimesCheckerTest.java
@@ -3,14 +3,12 @@ package com.rbenes;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest {
+
+public class PrimesCheckerTest {
 
     @Test
-    public void testApp()
-    {
+    public void testApp() {
+
         assertThat(1).isEqualTo(1);
     }
 }
