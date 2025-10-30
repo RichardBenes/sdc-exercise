7151f4f2d9f145b8ef73954ef321b80e06bb9da1
Richard Beneš <richard.benes.9@email.cz>
2025-10-30 15:40:42 +0100

    Update jUnit

--- src/test/java/com/rbenes/AppTest.java
+++ src/test/java/com/rbenes/AppTest.java
@@ -1,38 +1,16 @@
 package com.rbenes;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
+    @Test
     public void testApp()
     {
-        assertTrue( true );
+        assertThat(1).isEqualTo(1);
     }
 }
