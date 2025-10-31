fb8debaa7ebbfadd2e923d43eb3ba2dfd8ef6ddc
Richard Beneš <richard.benes.9@email.cz>
2025-10-31 10:46:27 +0100

    Read file name from cmd arg

--- src/main/java/com/rbenes/PrimesChecker.java
+++ src/main/java/com/rbenes/PrimesChecker.java
@@ -9,14 +9,19 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class PrimesChecker {
 
-    static final String FILENAME = "vzorek_dat - kopie.xlsx";
-
     // TODO: handle exceptions more gracefully
     // TODO: use logger instead of stdout
     // TODO: handle the file being used by some other process
     public static void main( String[] args ) throws InvalidFormatException, IOException {
+
+        if (args.length != 2) {
+            System.out.println("Expected file name as a single argument");
+            System.exit(-1);
+        }
+
+        var filename = args[1];
         
-        try (Workbook w = new XSSFWorkbook(new File(FILENAME))) {
+        try (Workbook w = new XSSFWorkbook(new File(filename))) {
 
             Sheet sheet = w.getSheetAt(0);
 
