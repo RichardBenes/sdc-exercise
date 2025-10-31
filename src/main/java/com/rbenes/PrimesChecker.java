2ea4b771993a1db7b24a1ab9824a84df5d2b6706
Richard Beneš <richard.benes.9@email.cz>
2025-10-31 10:46:08 +0100

    Read the cell in main(); create self-contained jar

--- src/main/java/com/rbenes/PrimesChecker.java
+++ src/main/java/com/rbenes/PrimesChecker.java
@@ -1,9 +1,30 @@
 package com.rbenes;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class PrimesChecker {
 
-    public static void main( String[] args ) {
+    static final String FILENAME = "vzorek_dat - kopie.xlsx";
 
-        System.out.println( "Hello Prime World!" );
+    // TODO: handle exceptions more gracefully
+    // TODO: use logger instead of stdout
+    // TODO: handle the file being used by some other process
+    public static void main( String[] args ) throws InvalidFormatException, IOException {
+        
+        try (Workbook w = new XSSFWorkbook(new File(FILENAME))) {
+
+            Sheet sheet = w.getSheetAt(0);
+
+            // Counting is zero based in Java, but 1-based in Excel...
+            Row row0 = sheet.getRow(1);
+            Cell cellB2 = row0.getCell(1);
+
+            System.out.println(cellB2.getStringCellValue());
+        }        
     }
 }
